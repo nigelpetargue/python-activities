0a1b8d1c368a612f7d239705475aa64dc35bce95
nigelpetargue <nigelpetargue@gmail.com>
2025-04-25 22:51:01 +0800

    initital commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Activities
+
+## Beginner Level
+
+### ✅ ~~Number Guessing Game~~
+
+**Core Concepts:**
+
+- 🖥️ Input and output
+- 📦 Variables
+- 🎲 Random number generation
+- 🔀 Conditional statements (`if`, `elif`, `else`)
+- 🔁 Basic loops (`while`)
+
+---
+
+### 📝 Mad Libs Generator
+
+**Core Concepts:**
+
+- 🖥️ Input and output
+- ✂️ String manipulation (concatenation, formatting)
+- 📦 Variables
+
+---
+
+### ✊ Rock, Paper, Scissors
+
+**Core Concepts:**
+
+- 🖥️ Input and output
+- 🎲 Random choice
+- 🔀 Conditional statements
+
+---
+
+### ➕ Basic Calculator
+
+**Core Concepts:**
+
+- 🖥️ Input and output
+- 📦 Variables
+- 🔀 Conditional statements
+- 🛠️ Potentially basic function definition
+
+---
+
+### 📋 To-Do List Application (Text-Based)
+
+**Core Concepts:**
+
+- 📜 Lists
+- 🔁 Loops
+- 🖥️ Input and output
+- 🧭 Basic program flow control
---
+++ beginner/number-guessing-game.py
@@ -0,0 +1,29 @@
+import random
+
+def number_guesssing():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    
+    random_number = random.randint(1, 100)
+    attempts = 0
+
+    print(f"Correct answer: {random_number}")
+
+    while True:
+        attempts += 1
+
+        try: 
+            guess = int(input("Take a guesss: "))
+            if guess > random_number: 
+                print("Too high!")
+            elif guess < random_number:
+                print("Too low!")
+            else:
+                print(f"Congratulations! You guessed the number {random_number} in {attempts} {"attempt!" if attempts == 1 else "attempts!"}")
+                break  
+        except ValueError:
+            print("Invalid input. Please enter a whole number.")
+
+    pass
+
+number_guesssing()
